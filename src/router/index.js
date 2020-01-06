99529b64802a6e572d727a8ed3316a6ff1e54f35
fxsong137 <xing__011@163.com>
2020-01-06 16:33:52 +0800

    修改API页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -30,107 +30,113 @@ import Layout from '@/layout'
  **/
 // constantRoutes:所有权限通用路由表  代表那些不需要动态判断权限的路由，如登录页、404、等通用页面。
 export const constantRoutes = [
-  {
-    // 根路由
-    path: '/',
-    component: Layout,
-    redirect: '/index',
-    children: [{
-      path: 'index',
-      name: 'Index',
-      component: () => import('@/views/index/index'),
-      meta: { title: '首页', icon: 'home' }
-    }]
-  },
-  /* 这里需要注意一下，一般侧边栏有两种形式即：submenu 和 直接 el-menu-item。 一个是嵌套子菜单，另一个则是直接一个链接。如下图：
-     在 Sidebar 中已经帮你做了判断，当你一个路由下面的 children 声明的路由大于>1 个时，自动会变成嵌套的模式。
-     如果子路由正好等于一个就会默认将子路由作为根路由显示在侧边栏中，
-     若不想这样，可以通过设置在根路由中设置alwaysShow: true来取消这一特性。如：
-  */
-  {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true// 不在侧边栏线上出现
-  },
+    {
+        // 根路由
+        path: '/',
+        component: Layout,
+        redirect: '/index',
+        children: [{
+            path: 'index',
+            name: 'Index',
+            component: () => import('@/views/index/index'),
+            meta: { title: '首页', icon: 'home' }
+        }]
+    },
+    /* 这里需要注意一下，一般侧边栏有两种形式即：submenu 和 直接 el-menu-item。 一个是嵌套子菜单，另一个则是直接一个链接。如下图：
+       在 Sidebar 中已经帮你做了判断，当你一个路由下面的 children 声明的路由大于>1 个时，自动会变成嵌套的模式。
+       如果子路由正好等于一个就会默认将子路由作为根路由显示在侧边栏中，
+       若不想这样，可以通过设置在根路由中设置alwaysShow: true来取消这一特性。如：
+    */
+    {
+        path: '/login',
+        component: () => import('@/views/login/index'),
+        hidden: true// 不在侧边栏线上出现
+    },
 
-  {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true// 不在侧边栏线上出现
-  },
-  {
-    path: '/user',
-    component: Layout,
-    redirect: '/user/userInfo',
-    name: 'User',
-    meta: { title: '个人中心', icon: 'user01' },
-    children: [
-      {
-        path: 'userInfo',
-        name: 'UserInfo',
-        component: () => import('@/views/user/userInfo'),
-        meta: { title: '详细信息', icon: 'table' }
-      },
-      {
-        path: 'realName',
-        name: 'RealName',
-        component: () => import('@/views/user/realName'),
-        meta: { title: '实名', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/api',
-    component: Layout,
-    redirect: '/api',
-    name: 'api',
-    children: [{
-      path: 'api',
-      name: 'Api',
-      component: () => import('@/views/api/index'),
-      meta: { title: 'API', icon: 'API' }
+    {
+        path: '/404',
+        component: () => import('@/views/404'),
+        hidden: true// 不在侧边栏线上出现
     },
     {
-      // API详情
-      path: 'details',
-      name: 'details',
-      component: () => import('@/views/api/details/apiDetails'),
-      meta: { title: '详情' },
-      hidden: true
+        path: '/user',
+        component: Layout,
+        redirect: '/user/userInfo',
+        name: 'User',
+        meta: { title: '个人中心', icon: 'user01' },
+        children: [
+            {
+                path: 'userInfo',
+                name: 'UserInfo',
+                component: () => import('@/views/user/userInfo'),
+                meta: { title: '详细信息', icon: 'table' }
+            },
+            {
+                path: 'realName',
+                name: 'RealName',
+                component: () => import('@/views/user/realName'),
+                meta: { title: '实名', icon: 'tree' }
+            }
+        ]
+    },
+    {
+        path: '/api',
+        component: Layout,
+        redirect: '/api/api-index',
+        name: 'api',
+        meta: { title: 'API', icon: 'API' },
+        children: [
+            {
+                // API详情
+                path: 'api-index',
+                name: 'Api-index',
+                component: () => import('@/views/api/index'),
+                meta: { title: '列表' },
+                hidden: true
 
-    }]
-  },
-  {
-    path: '/cost',
-    component: Layout,
-    redirect: '/cost/balance',
-    name: 'Cost',
-    meta: { title: '费用', icon: 'feiyong' },
-    children: [
-      {
-        path: 'balance',
-        name: 'Balance',
-        component: () => import('@/views/cost/balance'),
-        meta: { title: '余额', icon: 'table' }
-      },
-      {
-        path: 'order',
-        name: 'Order',
-        component: () => import('@/views/cost/order'),
-        meta: { title: '充值', icon: 'tree' }
-      },
-      {
-        path: 'invoice',
-        name: 'Invoice',
-        component: () => import('@/views/cost/invoice'),
-        meta: { title: '发票', icon: 'tree' }
-      }
-    ]
-  },
+            },
+            {
+                // API详情
+                path: 'details',
+                name: 'details',
+                component: () => import('@/views/api/details/apiDetails'),
+                meta: { title: '详情' },
+                hidden: true
 
-  // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
+            },
+        ]
+
+    },
+    {
+        path: '/cost',
+        component: Layout,
+        redirect: '/cost/balance',
+        name: 'Cost',
+        meta: { title: '费用', icon: 'feiyong' },
+        children: [
+            {
+                path: 'balance',
+                name: 'Balance',
+                component: () => import('@/views/cost/balance'),
+                meta: { title: '余额', icon: 'table' }
+            },
+            {
+                path: 'order',
+                name: 'Order',
+                component: () => import('@/views/cost/order'),
+                meta: { title: '充值', icon: 'tree' }
+            },
+            {
+                path: 'invoice',
+                name: 'Invoice',
+                component: () => import('@/views/cost/invoice'),
+                meta: { title: '发票', icon: 'tree' }
+            }
+        ]
+    },
+
+    // 404 page must be placed at the end !!!
+    { path: '*', redirect: '/404', hidden: true }
 ]
 
 // 动态清除注册的路由
@@ -138,17 +144,17 @@ export const constantRoutes = [
 // 将它的matcher重新赋值给我们之前定义的路由就可以了。巧妙的实现了动态路由的清除。
 // 现在我们只需要调用resetRouter，就能得到一个空的路有实例，之后你就可以重新addRoutes你想要的路由了
 const createRouter = () => new Router({
-  // 模式mode: 'history', // 需要浏览器服务支持
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
+    // 模式mode: 'history', // 需要浏览器服务支持
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
 })
 
 const router = createRouter()
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+    const newRouter = createRouter()
+    router.matcher = newRouter.matcher // reset router
 }
 /* END 动态清除路由 END */
 export default router
